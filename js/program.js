0060b97b4165bb1e25e0a10585754e6a778cc8bf
scmstr <55764330+scmstr@users.noreply.github.com>
2020-01-20 23:47:59 -0800

    Add files via upload
    its made now

--- js/program.js
+++ js/program.js
@@ -1,74 +1,58 @@
-// Arrow key codes
-var UP = 38,
-    DOWN = 40,
-    RIGHT = 39,
-    LEFT = 37;
-
-// rocket object
-var rocket = {
-	img: document.querySelector("#rocket"),
-	x: 490,
-	y: 390,
-	width: 100
-};
-var velocity = 8;
-
-// make local names just to save typing later
-var torpedo = document.querySelector("#torpedo"),
-    startBtn = document.querySelector("#start"),
-    fireBtn = document.querySelector("#fire"),
-    ufo = document.querySelector("#ufo");
+let page1 = document.getElementById("page1");
+let page2 = document.getElementById("page2");
+let switchButton = document.getElementById("switchButton");
+let forwards = document.getElementById("forwards");
+let array1 = ["hi", "hi", "hi"];
+let array2 = ["bye", "bye", "bye"];
+let list1 = document.getElementById("list1");
+let list2 = document.getElementById("list2");
 
 
-// Initialize objects on the screen  (call method defined below)
-render ( );
+window.onloadend = AskQuestions();
 
-// define what should happen when click each of the 3 buttons
-startBtn.addEventListener("click",startGameHandler,false);
-fireBtn.addEventListener("click",fireTorpedoHandler,false)
-window.addEventListener("keydown",keydownHandler,false);
-
-function startGameHandler( ) {
-	// Hide the intro screen, show the game screen
-	introScreen.style.display = "none";
-	gameScreen.style.display = "block";
-	rocket.img.style.display = "block";
-	ufo.style.display = "block";
-}
-
-function fireTorpedoHandler( ) {
-	// Fire the photon torpedo!
-	// CSS animation occurs whenever torpedo
-	// 'left' property changes value
-	torpedo.style.visibility = "visible";
-	torpedo.style.left = (rocket.x - 400)+ "px";
-}
-
-function keydownHandler(event) {
-	// handle user keyboard input
-    // modify the property value of the rocket objects by the amount velocity it set to
-	if (event.keyCode == UP) {
-		rocket.y -= velocity;
+function AskQuestions() {
+	let temp = "";
+	for(i = 0; i < 3; i++) {
+		array1[i] = window.prompt("enter a word: ");
 	}
-	if (event.keyCode == LEFT) {
-		rocket.x -= velocity;
-	}
-	if (event.keyCode === DOWN) {
-		rocket.y += velocity;
-	}
-	if (event.keyCode == RIGHT) {
-		rocket.x += velocity;
+	
+	for(i = 0; i < array1.length; i++) {
+		let thing = document.createElement("li");
+		thing.innerHTML = array1[i];
+		list1.appendChild(thing);
 	}
 
-	render( );  // call render again after moving things
+	array2 = array1.map(FlipLetters);
 }
 
-function render( ) {
-	// position objects on the screen, form pf location is
-	// rocket.img.style.left = 320px     so use code to make that
-	rocket.img.style.left = rocket.x + "px";
-	rocket.img.style.top = rocket.y + "px";
-	torpedo.style.left = (rocket.x +10) + "px";
-	torpedo.style.top = (rocket.y+8) + "px";
-	torpedo.style.visibility = "hidden";
+function Switch() {
+	page1.style.display = "none";
+	page2.style.display = "block";
+
+	for(i = 0; i < array2.length; i++) {
+		let thing = document.createElement("li");
+		thing.innerHTML = array2[i];
+		list2.appendChild(thing);
+	}
+
 }
+
+function FlipLetters(string) {
+	let cake = [];
+	let j = "";
+	let k = [];
+	let output = "";
+
+	j = string;
+	cake = j.split("");
+	k = cake[0]
+	cake[0] = cake[(cake.length)-1]
+	cake[((cake.length)-1)] = k;
+	output = cake.join("");
+
+	return output;
+}
+
+switchButton.addEventListener("click", Switch);
+
+
